7d7016d9fdf9ecf2b8bf44fcc853711762acb1a0
Timothy Portfolio <tportfolio@ucla.edu>
2020-03-05 01:03:09 -0800

    add express; add electron builder (still needs to resolve correct for express in prod

--- public/electron.js
+++ public/electron.js
@@ -1,17 +1,25 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const child_process = require('child_process');
+
+// const server = require('./server'); //TODO: split out code to frontend/backend
+// console.log("found server");
+// console.log(server);
 
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
+const url = require('url');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  mainWindow.on('closed', () => mainWindow = null);
+	mainWindow = new BrowserWindow({width: 900, height: 680});
+	mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+	mainWindow.setMenuBarVisibility(isDev);
+	child_process.fork(isDev ? path.resolve(__dirname, "../public/server.js") : path.resolve(__dirname, "server.js"));
+
+	mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import path from 'path';
 import Sidebar from "./sidebar/Sidebar";
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { blue, indigo } from '@material-ui/core/colors';
@@ -21,17 +22,58 @@ const theme = createMuiTheme({
   }
 });
 
-function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <div className="App">
-                <Sidebar />
-                <p>
-                    Some boilerplate to verify Electron is running with live server.
-                </p>
-            </div>
-        </ThemeProvider>
-    );
+
+class App extends React.Component {     
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+  
+    componentDidMount() {
+        // Call our fetch function below once the component mounts
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    };
+
+      // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callBackendAPI = async () => {
+      const response = await fetch(path.join(__dirname, '/express_backend'));
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
+
+  //   const onChangeHandler=event=>{
+	// const images = require.context('../../public/images', true);
+
+  //     for (const file of event.target.files) {
+  //       console.log(file);
+    
+	//   }   
+	//   setImg(event.target.files[0]);
+  //   }
+    
+render() {
+      return (
+          <ThemeProvider theme={theme}>
+              <div className="App">
+                  <Sidebar />
+                  <p>
+                      Some boilerplate to verify Electron is running with live server.
+                  </p>
+                  <p>{this.state.data}</p>
+                  <input directory="" webkitdirectory="" type="file" onChange={() => console.log('hello')}/>
+              </div>
+          </ThemeProvider>
+      );
+    }
 }
 
 export default App;
