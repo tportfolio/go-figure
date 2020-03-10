d3307f834f97ac0b1ce963f4fb56ef4dcbe47911
Timothy Portfolio <tportfolio@ucla.edu>
2020-03-10 00:50:24 -0700

    add click toggle, fix redux bug

--- src/components/ModifiableImage.js
+++ src/components/ModifiableImage.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Rnd } from 'react-rnd';
-import { updatePictureState } from "../store/reducer";
+import { updatePictureState, togglePictureSelection } from "../store/reducer";
 import { base64StringToBlob } from 'blob-util';
+import "./image.css";
+import classNames from "classnames";
+import isEqual from "lodash/isEqual";
 
 const ModifiableImage = props => {
-    const { data, cachedProperties, updateProperties } = props;
+    const { data, cachedProperties, selectedPictures, updateProperties, togglePictureSelection } = props;
     const { dimensions: cachedDimensions } = cachedProperties;
-    const cachedPosition = cachedProperties.position ? cachedProperties.position : { x: 0, y: 0 };
     const [dimensions, setDimensions] = React.useState(cachedDimensions ? cachedDimensions : { height: 0, width: 0 });
-
+    const cachedPosition = cachedProperties.position ? cachedProperties.position : { x: 0, y: 0 };
+    
+    const ref = React.useRef();
     const onLoad = target => setDimensions({ height: target.height, width: target.width });
+
     const blob = React.useMemo(() => URL.createObjectURL(base64StringToBlob(data, "image/png")), [data]);
-    let imgProps = React.useMemo(() => ({ draggable: false, src: blob, onLoad: e => onLoad(e.target) }), [blob]);
+    let imgProps = React.useMemo(() => ({ ref: ref, src: blob, onLoad: e => onLoad(e.target) }), [blob]);
 
     const { height, width } = dimensions;
     if (height && width) {
         imgProps = { ...imgProps, height: height, width: width };
     }
 
+    const className = classNames({ selected: selectedPictures.includes(data) });
+
+    const onDragStop = (e, position) => {
+         if (position.x === cachedPosition.x && position.y === cachedPosition.y) {
+            togglePictureSelection(data);
+        } else {
+            updateProperties(data, { position: { x: position.x, y: position.y } });
+        }
+    }
+
     return (
-        <Rnd style={{ zIndex: 9 }} lockAspectRatio={true} default={cachedPosition}
-            onResize={(e, direction, ref, delta, position) => {
-                setDimensions({ height: ref.style.height, width: ref.style.width });
-            }}
-            onResizeStop={(e, direction, ref, delta, position) => {
-                updateProperties(data, { dimensions: { height: ref.style.height, width: ref.style.width }, position: position });
-            }}
-            onDragStop={(e, position) => {
-                updateProperties(data, { position: { x: position.x, y: position.y } });
-            }}
-        >
-            <img {...imgProps} />
+            <Rnd style={{ zIndex: 9 }} lockAspectRatio={true} default={cachedPosition}
+                onResize={(e, direction, ref, delta, position) => {
+                    setDimensions({ height: ref.style.height, width: ref.style.width });
+                }}
+                onResizeStop={(e, direction, ref, delta, position) => {
+                    updateProperties(data, { dimensions: { height: ref.style.height, width: ref.style.width }, position: position });
+                }}
+                onDragStop={onDragStop}
+            >
+        <img {...imgProps} draggable={false} className={className}
+        />
         </Rnd>
     );
 };
@@ -39,13 +53,15 @@ const ModifiableImage = props => {
 const mapStateToProps = (state, ownProps) => {
     const cachedProperties = state.pictures[ownProps.data];
     return {
-        cachedProperties: cachedProperties ? cachedProperties : {}
+        cachedProperties: cachedProperties ? cachedProperties : {},
+        selectedPictures: state.selectedPictures
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        updateProperties: (data, properties) => dispatch(updatePictureState(data, properties))
+        updateProperties: (data, properties) => dispatch(updatePictureState(data, properties)),
+        togglePictureSelection: data => dispatch(togglePictureSelection(data))
     }
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,12 @@
+import pull from "lodash/pull";
+
 const ADD_PICTURE_ACTION = "ADD_PICTURE";
 const UPDATE_PICTURE_STATE = "UPDATE_PICTURE_STATE";
+const TOGGLE_PICTURE_SELECTION = "TOGGLE_PICTURE_SELECTION";
 
 const initialState = {
-    pictures: {}
+    pictures: {},
+    selectedPictures: []
 };
 
 export const managePictures = (state = initialState, action) => {
@@ -10,17 +14,39 @@ export const managePictures = (state = initialState, action) => {
         case ADD_PICTURE_ACTION:
             return {
                 ...state,
-                pictures: {...state.pictures, [action.payload.data]: {}}
+                pictures: { ...state.pictures, [action.payload.data]: {} }
             }
         case UPDATE_PICTURE_STATE:
             return {
                 ...state,
-                pictures : {...state.pictures, [action.payload.data]: Object.assign(state.pictures[action.payload.data], action.payload.updatedProperties)}
+                pictures: { ...state.pictures, [action.payload.data]: Object.assign({...state.pictures[action.payload.data]}, action.payload.updatedProperties) }
+            }
+        case TOGGLE_PICTURE_SELECTION:
+            const {data} = action.payload;
+            let result;
+            if (state.selectedPictures.includes(data)) {
+                result = pull([...state.selectedPictures], data);
+            } else {
+                result = [...state.selectedPictures, action.payload.data];
+            }
+            return {
+                ...state,
+                selectedPictures: result
             }
         default:
             return state;
     }
 }
+
+export const togglePictureSelection = data => {
+    return {
+        type: TOGGLE_PICTURE_SELECTION,
+        payload: {
+            data: data
+        }
+    }
+}
+
 
 export const addPicture = data => {
     return {
