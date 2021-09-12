f9d6b87b386050cc9caf6b03a18307d96e4fb18f
Timothy Portfolio <timothymportfolio@gmail.com>
2021-09-11 22:59:26 -0700

    Animate title screen with additional menu toggle hint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Fade } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import BrushIcon from '@material-ui/icons/Brush';
 import './App.css';
+
+const TITLE_TRANSITION_DURATION_MSECS = 3000;
 
 const App = () => {
     return (
-        <div className="App">
-            <p>
-                Some boilerplate to verify Electron is running with live server.
-			</p>
-        </div>
+        <Grid container direction="column">
+            <Fade in={true} timeout={TITLE_TRANSITION_DURATION_MSECS}>      
+                <Grid container direction="row" justify="center" alignItems="center">
+                    <Grid item>                  
+                        <p className="splash-screen-title">
+                            go figure
+                        </p>
+                        </Grid>  
+                        <Grid item>
+                        <BrushIcon className="paint-brush-icon" style={{color: "white", fontSize: "96px"}}/>
+                    </Grid>    
+                </Grid>
+            </Fade>
+            <Grid container direction="row" justify="center">
+                <Grid item>
+                    <Fade in={true} style={{ transitionDelay: TITLE_TRANSITION_DURATION_MSECS + 'ms'}} timeout={TITLE_TRANSITION_DURATION_MSECS}>
+                        <p className="left-arrow-hint">
+                            (press left/right arrow keys to toggle menu)
+                        </p>
+                    </Fade>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
